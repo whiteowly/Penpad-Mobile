e80670fc54caf262ad73b1efbcb25d48c659f911
whiteowly <jerryayalew696@gmail.com>
2025-10-10 19:50:42 +0300

    configured the backend

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,8 +1,21 @@
-import {Client, Account, Avatars} from 'react-native-appwrite';
+import {Client, Databases} from 'react-native-appwrite';
 
-export const client = new Client()
-  .setProject('68dc1f8c002b40670129') // Your project ID
-  .setPlatform('com.jer.todolisss'); // Your app package name
+const config = {
+  endpoint: "https://fra.cloud.appwrite.io/v1",
+  projectId: "68dc1f8c002b40670129",
+  db: "todolisss",
+  col:{
+    tasks: "tasks",
+  },
 
-export const account = new Account(client);
-export const avatars = new Avatars(client);
+
+};
+
+const client = new Client()
+  .setEndpoint(config.endpoint) 
+  .setProject(config.projectId); 
+
+ const databases = new Databases(client);
+export { client, databases, config };
+
+  
